1d7dde18fea0411a1915c36947c931b2f72f3214
C14H22O <160833462+C14H22O@users.noreply.github.com>
2024-03-13 22:51:53 +0800

    添加读取本地插件

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ driver = nonebot.get_driver()
 nonebot.get_driver().register_adapter(Adapter)
 
 nonebot.load_from_toml("pyproject.toml")
+nonebot.load_plugins("./src/plugins")
 
 if __name__ == "__main__":
     nonebot.run()
