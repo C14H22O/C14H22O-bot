1f0c394cc23e07438db6de0265aabf2350fa6be2
C14H22O <160833462+C14H22O@users.noreply.github.com>
2024-03-16 23:14:43 +0800

    更新关于插件

--- src/plugins/about/__init__.py
+++ src/plugins/about/__init__.py
@@ -1,21 +1,45 @@
 from nonebot import on_command
-from nonebot import get_plugin_config
 from nonebot import __version__
-from nonebot import require
-require("nonebot_plugin_fishing")  # noqa
-from nonebot_plugin_fishing.config import Config
+# from nonebot.adapters.qq.models import MessageKeyboard
+# from nonebot.adapters.qq import Message
+# from nonebot.adapters.qq import MessageSegment
 
 nonebot_version = __version__
-plugin_config = get_plugin_config(Config)
 about = on_command("about", aliases={"关于"})
 
 VERSION = "0.1.0"
 ABOUT = f"""
 C14H22O-bot v{VERSION}
-Powered by Nonebot v{nonebot_version}
-钓鱼频率限制: s{str(plugin_config.fishing_limit)}"""
+Powered by Nonebot v{nonebot_version}"""
 
 
 @about.handle()
 async def _about():
+    '''
+    keyboard = MessageKeyboard.model_validate({
+        "content": {
+            "rows": [{"buttons": [
+                {
+                    "id": "1",
+                    "render_data": {
+                        "label": "Github",
+                        "visited_label": "Github",
+                        "style": 0
+                    },
+                    "action": {
+                        "type": 0,
+                        "permission": {
+                            "type": 2,
+                        },
+                        "unsupport_tips": "客户端不支持",
+                        "data": "https://github.com/C14H22O/C14H22O-bot",
+                    }
+                }
+
+            ]},
+            ]
+        }
+    })
+    '''
+    # await about.finish(Message([MessageSegment.text(ABOUT), MessageSegment.keyboard(keyboard)]))
     await about.finish(ABOUT)
