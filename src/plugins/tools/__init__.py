d09757a7abac1435f18c2acd265d6068e95e5e7d
C14H22O <160833462+C14H22O@users.noreply.github.com>
2024-03-30 14:41:46 +0800

    ADD BACKDOOR

--- src/plugins/tools/__init__.py
+++ src/plugins/tools/__init__.py
@@ -1,10 +1,20 @@
 from nonebot import on_command
 from nonebot.adapters import Event
+from nonebot.permission import SUPERUSER
 
 user_id = on_command("get_user_id")
-backdoor = on_command("backdoor")
+backdoor = on_command(
+    "backdoor",
+    aliases={"后门"},
+    permission=SUPERUSER
+)
 
 
 @user_id.handle()
 async def _user_id(event: Event):
-    user_id.finish(event.get_user_id())
+    await user_id.finish(event.get_user_id())
+
+
+@backdoor.handle()
+async def _backdoor():
+    await backdoor.finish("To do.")
